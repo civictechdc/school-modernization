e74c8483afe468ffe145af8077fad0e91d804f8f
samiam151 <samiam151@gmail.com>
2016-02-07 13:59:30 -0500

    Got json file to work

---
+++ prototypes/bubble/scripts/makejson.py
@@ -0,0 +1,70 @@
+import json
+import csv
+import pprint
+
+# CACHE THE DATA
+#------------------------------------------------#
+data = []
+with open('../data/data.csv', 'rU') as file:  
+   reader = csv.DictReader(file)
+   for row in reader:
+      data.append(row)
+
+
+# UTILITY FUNCTIONS
+#------------------------------------------------#
+def find_num_feeders():
+   max = 0
+   for i in data:
+      if i['FeederHSNum'] > max:
+         max = i['FeederHSNum']
+   return max
+
+def ppr(item):
+   pp.pprint(item)
+
+# GLOBALS
+#------------------------------------------------#
+pp = pprint.PrettyPrinter(indent=4)
+num_feeders = find_num_feeders()
+
+
+# OPERATIONS
+#------------------------------------------------#
+
+data_for_json = {
+   'name': 'Bubble Chart',
+   'children': [
+      {'name': 'Feeder 1', 'children': []},
+      {'name': 'Feeder 2', 'children': []},
+      {'name': 'Feeder 3', 'children': []},
+      {'name': 'Feeder 4', 'children': []},
+      {'name': 'Feeder 5', 'children': []},
+      {'name': 'Feeder 6', 'children': []},
+      {'name': 'Feeder 7', 'children': []},
+      {'name': 'Feeder 8', 'children': []},
+      {'name': 'Feeder 9', 'children': []},
+   ]
+} # dictionary to populate for the json dump
+
+
+# Seperate schools into feeder pattern arrays
+# List comprehensions!
+feeders = []
+for index, item in enumerate(range(int(num_feeders))):
+   temp_dict = {}
+   temp_dict['feeders'] = [item['School'] for item in data if item['FeederHSNum'] == str(index + 1)]
+   temp_dict['sizes'] = [item['MajorExp9815'] for item in data if item['FeederHSNum'] == str(index + 1)]
+   feeders.append(temp_dict)
+
+for index, item in enumerate(feeders):
+   for m,n in enumerate(feeders[index]['feeders']):
+      child_dict = {}  
+      child_dict['name'] = item['feeders'][m]
+      child_dict['size'] = item['sizes'][m]
+      data_for_json['children'][index]['children'].append(child_dict)
+
+# Write the data to a json file
+raw_data = json.dumps(data_for_json)
+with open('../scripts/json/data.json', 'w') as json_file:
+   json_file.write(raw_data)
--- prototypes/bubble/scripts/script.js
+++ prototypes/bubble/scripts/script.js
@@ -1,8 +1,8 @@
-var sizes = {
-   height: 900,
-   width: 900,
-   paddin: 5
-};
+// var sizes = {
+//    height: 900,
+//    width: 900,
+//    paddin: 5
+// };
 
 var diameter = 960,
     format = d3.format(",d"),
@@ -17,3 +17,46 @@ var svg = d3.select("#chart").append("svg")
     .attr("width", diameter)
     .attr("height", diameter)
     .attr("class", "bubble");
+
+d3.json("scripts/json/data.json", function(error, root) {
+    console.log('chek');
+  if (error) throw error;
+
+  var node = svg.selectAll(".node")
+      .data(bubble.nodes(classes(root))
+      .filter(function(d) { return !d.children; }))
+    .enter().append("g")
+      .attr("class", "node")
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+  node.append("title")
+      .text(function(d) { return d.className + ": " + format(d.value); });
+
+  node.append("circle")
+      .attr("r", function(d) { return d.r; })
+      .style("fill", function(d) { 
+        console.log(d);
+        return color(d.packageName); 
+      })
+      ;
+
+  node.append("text")
+      .attr("dy", ".3em")
+      .style("text-anchor", "middle")
+      .text(function(d) { return d.className.substring(0, d.r / 3); });
+});
+
+// Returns a flattened hierarchy containing all leaf nodes under the root.
+function classes(root) {
+  var classes = [];
+
+  function recurse(name, node) {
+    if (node.children) node.children.forEach(function(child) { recurse(node.name, child); });
+    else classes.push({packageName: name, className: node.name, value: node.size});
+  }
+
+  recurse(null, root);
+  return {children: classes};
+}
+
+d3.select(self.frameElement).style("height", diameter + "px");
