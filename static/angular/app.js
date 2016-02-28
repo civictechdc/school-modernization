356130f369c6cb982d169dc35e95c0549d537194
Nick Skelsey <nskelsey@gmail.com>
2016-02-28 01:03:30 -0500

    Updated site with a proper structure and functional css.

--- static/angular/app.js
+++ static/angular/app.js
@@ -1,20 +1,36 @@
 'use strict';
 
 angular.module("PostModern", ['ngRoute'])
-  .config(["$routeProvider", function($routeProvider) {
+  .config(["$routeProvider", function($routeProvider, navService) {
       console.log("ang:routeProvider");
       $routeProvider.when("/story/:name", {
           controller: 'storyCtrl',
-          templateUrl: 'static/angular/story.html',
+          templateUrl: '/static/angular/story.html',
+      })
+      .when("/intro", {
+          templateUrl: '/static/angular/intro.html',
+      })
+      .when('/appendix', {
+          templateUrl: '/static/angular/appendix.html',
+      })
+      .when('/about', {
+          templateUrl: '/static/angular/about.html',
       })
   }])
   .controller('storyCtrl', function($scope, navService, storyService, mapService) {
     console.log("ang:storyCtrl");
-    var path = navService.GetPath();
-    var story = storyService[path.path];
-    $scope.story = story;
-    story.mapDisplay(mapService);
+    var navObj = navService.GetPath();
+    var story = storyService[navObj.path];
+    var templatePath = '/static/angular/' + navObj.path;
+    $scope.story = story
+    $scope.storyName = navObj.path;
+    $scope.belowFold = templatePath + '.html';
+    if (story.includeTop) {
+      $scope.includeTop = story.includeTop;
+      $scope.titlePara = templatePath + '-top.html';
+    }
 
+    story.mapDisplay(mapService);
   })
   .controller('navCtrl', function($scope, navService) {
     console.log("ang:navCtrl");
@@ -33,7 +49,6 @@ angular.module("PostModern", ['ngRoute'])
 
     var currentIdx = 0;
     var paths = [
-      { path: 'intro', text: "Introduction"},
       { path: 'dig-out', text: "Digging Out"},
       { path: 'dig-in', text: "Modernized"},
       { path: 'branch-off', text: "Charters Built"},
@@ -70,34 +85,40 @@ angular.module("PostModern", ['ngRoute'])
         para: "Since 1998, Washington school facilities have gone under enormous transformation, with more than $4 billion spent on district and charter schools.\nUnfortunately, spending data has not been available until now.\nThe aim of this site is to provide a holistic view of how Washington has allocated its resources and to bring historical perspective to future budget discussions."
       },
       "dig-out": {
-        title: "The DC Government Spent $1.2 Billion to Bail the Public Schools Out",
-        para: "Beginning in the late 1980's, Washington school facilities were decrepit and unfit for daily operations.\nStarting in 1998 and over the next 15 years, the city would spend more than $1B for basic building needs to eliminate its backlog of unhealthy, unsafe and unworkable conditions.  Projects included replacing roofs, installing HVAC systems, replacing windows and building security systems among other projects.\nThis initial $1B investment in basic facility needs would set the stage for full-scale modernization of our public schools.",
+        title: "Digging Out: Stabilizing DCPS Facilities",
+        para: "In 1995, DCPS buildings and grounds were unhealthy, unsafe, unpleasant, and under-utilized.  The accumulation of deferred maintenance was est imated at $1.2 billion in 2015 dollars. From 1999-2015 the D.C. Council appropriated $1 billion to bring DCPS facilities up to a minimum standard and reduce the amount of under utilized space in DCPS schools that had resulted from dramatic city population decline.",
         mapDisplay: function(filterSelector) {
             filterSelector("District", null, null, "Ward", "spendAmount", "storyMap");
         },
       },
       "dig-in": {
-        title: "Between 2002 and 2015 Over 2.6 Billion was spent Modernizing Public Schools",
-        para: "While instituting these basic building needs, the city embarked on a major modernization program, which would expend $2.6B through 2015.",
+        title: "Progress: DCPS Modernizations 2000-2015",
+        para: "Using 21st century facilities standards for health, sustainability, education and community use, DCPS embarks upon an unprecedented modernization program.",
         mapDisplay: function(filterSelector) {
             filterSelector("District", null, "spendPast", "Ward", "spendAmount", "storyMap");
         },
       },
       "branch-off": {
-        title: "In 1998 The Charter School System Lauched in DC",
-        para: "Decades of disinvestment before 2000 contributed to the decision for many families to choose charter schools. Starting in 1998 charter schools secured a per student facility allowance, allowying them to lease, buy and improve facilities. Since 1998, charter schools have received $963 million from the District.",
+        includeTop: true,
         mapDisplay: function(filterSelector) {
             filterSelector("Charter", null, null, "Ward", "spendAmount", "storyMap");
         },
       },
       "planned": {
-        title: "The DC Government plans to Spend 1.7 Billion on School Facilities",
-        para: "Decades of disinvestment before 2000 contributed to the decision for many families to choose charter schools. Starting in 1998 charter schools secured a per student facility allowance, allowying them to lease, buy and improve facilities. Since 1998, charter schools have received $963 million from the District.",
+        includeTop: true,
         mapDisplay: function(filterSelector) {
-            // filterSelector("District", null, "spendPlanned", "Ward", "storyMap");
             filterSelector("District", null, "spendPlanned", "Ward", "spendSqFt", "storyMap");
         },
+        bubbleDisplay: function(selector) {
+          // make_plan_bubbles(selector); 
+          // Parameters to pass in
+          // 1) Type of spend (column)
+          // 2) Public, Charter, or Both
+          // 3) Categories (split by level, agency, ward, feeders)
+        }
       }
     }
     return stories;
   });
+
+// Document progress
