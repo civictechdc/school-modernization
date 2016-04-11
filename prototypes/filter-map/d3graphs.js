296a411f23082d0a2e49d28da26ff71528fba185
tomBeach <tbeach2k@gmail.com>
2016-04-11 06:53:10 -0400

    added back top space positioning

--- prototypes/filter-map/d3graphs.js
+++ prototypes/filter-map/d3graphs.js
@@ -589,7 +589,7 @@ function makeRankChart(zonesCollectionObj, schoolsCollectionObj, displayObj, zon
           // Do nothing
         } else {
             $("#chart-container").css("position", "absolute");
-            $("#chart-container").css("top", "340px");
+            $("#chart-container").css("top", "410px");
             $("#chart-container").css("left", "20px");
             $("#chart-container").css("width", "520px");
             $("#chart-container").css("height", "auto");
