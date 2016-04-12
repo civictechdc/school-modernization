727b155c9e4d0e4ed9547e899c9c8d850e2d77e5
samiam151 <samiam151@gmail.com>
2016-04-12 19:16:43 -0400

    Initial Commit

---
+++ prototypes/bubble/script2.js
@@ -0,0 +1,5 @@
+d3.csv('https://cdn.rawgit.com/codefordc/school-modernization/master/Output%20Data/DCSchools_FY1415_Master_46.csv', function(data){
+    var bubble = new Bubble('LifetimeBudget'); // data
+
+    
+});
