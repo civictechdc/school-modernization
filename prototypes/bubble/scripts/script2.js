8f1e6a63f3fb6ec2aa327e0e1a291e44da17be23
samiam151 <samiam151@gmail.com>
2016-04-13 03:10:38 -0400

    Added back older menu conditions; final?

--- prototypes/bubble/scripts/script2.js
+++ prototypes/bubble/scripts/script2.js
@@ -1,24 +1,25 @@
 d3.csv('data/data_final.csv', function(data){
     var bubble = new Bubble(),
         schools = {
-        both: data,
-        public: (function(d){
-            return d.filter(function(item){
-                if (item.Agency === 'DCPS'){
-                    return item;
-                }
-            });
-        }(data)),
-        charter: (function(d){
-            return d.filter(function(item){
-                if (item.Agency === 'PCS'){
-                    return item;
-                }
-            });
-        }(data))
-            },
+            both: data,
+            public: (function(d){
+                return d.filter(function(item){
+                    if (item.Agency === 'DCPS'){
+                        return item;
+                    }
+                });
+            }(data)),
+            charter: (function(d){
+                return d.filter(function(item){
+                    if (item.Agency === 'PCS'){
+                        return item;
+                    }
+                });
+            }(data))
+        },
         initialBudgetState = 'future',
         budgetState = null,
+        isAltProjectType = false;
         perState = 'total';
 
     // Initial
@@ -28,7 +29,13 @@ d3.csv('data/data_final.csv', function(data){
     // States
     $('.budgetChange').each(function(){
         $(this).on('click', function(e){
-
+            clearInactive();
+            if (isSelected($('#ProjectType')) && e.target.id === 'future'){
+                bubble.setColumn('FUTUREProjectType16_21');
+            }
+            if (isSelected($('#ProjectType')) && e.target.id !== 'future'){
+                bubble.setColumn('ProjectType');
+            }
             var state = $(e.target).data(perState);
             bubble.setBudget(state);
             console.log('state:' + state);
@@ -42,6 +49,7 @@ d3.csv('data/data_final.csv', function(data){
     // Only row that doesnt work
     $('.perChange').each(function(){
         $(this).on('click', function(e){
+            clearInactive();
             var state = $(e.target).data(budgetState);
             perState = $(e.target).data('key');
             bubble.setBudget(state);
@@ -51,13 +59,30 @@ d3.csv('data/data_final.csv', function(data){
 
     $('.columnChange').each(function(){
         $(this).on('click', function(e){
-            bubble.setColumn(e.target.id);
+            clearInactive();
+            if (e.target.id === 'FeederHS'){
+                bubble.setData(schools.public);
+                makeInactive([$('#charter'), $('#both')]);
+            }
+            if (e.target.id === 'Agency'){
+                bubble.setData(schools.both);
+                makeInactive([$('#charter'), $('#public')]);
+            }
+            if (e.target.id === 'ProjectType' && budgetState === 'future'){
+                bubble.setColumn(e.target.dataset.alt);
+                isAltProjectType = true;
+                bubble.getColumn();
+            } else {
+                bubble.setColumn(e.target.id);
+            }
+            
             update(e);
         });
     });
 
     $('.schoolChange').each(function(){
         $(this).on('click', function(e){
+            clearInactive();
             bubble.setData(schools[e.target.id]);
             update(e);
         });
@@ -89,6 +114,7 @@ d3.csv('data/data_final.csv', function(data){
     function update(e){
         console.log('budgetState: ' + budgetState);
         console.log('perState: ' + perState);
+
         bubble.change();
         makeSelected(e);
     }
@@ -101,6 +127,28 @@ d3.csv('data/data_final.csv', function(data){
             $(e.target).addClass('selected');
             $(e.target).siblings().removeClass('selected');
         }   
+    }
+
+    function makeInactive(arr){;
+        if (arr.length > 1){
+            arr.forEach(function(item){
+                $(item).addClass('invalid');
+            });
+        } else {
+            $(arr).addClass('invalid');
+        }    
+    }
+
+    function clearInactive(){
+        console.log('done');
+        var $invalidsButtons = $('.invalid');
+        $.each($invalidsButtons, function(i){
+            $($invalidsButtons[i]).removeClass('invalid');
+        });        
+    }
+
+    function isSelected(el){
+        return $(el).hasClass('selected') ? true : false;
     }
 
 });
